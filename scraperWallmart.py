e5c467d2dfd5e791634d07d9a5e36c4548582e9a
marcelo <mhosan@gmail.com>
2021-11-10 20:17:01 -0300

    nada

--- scraperWallmart.py
+++ scraperWallmart.py
@@ -3,9 +3,16 @@ import driver
 
 driver = driver.seteoDriver()
 
-urls = ['https://www.walmart.com.ar/buscar?text=yerba',
-        'https://www.walmart.com.ar/buscar?text=leche'
+urls = ['https://www.walmart.com.ar/buscar?text=leche',
+    'https://www.walmart.com.ar/buscar?text=yerba',
+    'https://www.walmart.com.ar/buscar?text=azucar',
+    'https://www.walmart.com.ar/buscar?text=galletitas',
+    'https://www.walmart.com.ar/buscar?text=gaseosas',
+    'https://www.walmart.com.ar/buscar?text=shampoo',
+    'https://www.walmart.com.ar/buscar?text=arroz',
+    'https://www.walmart.com.ar/buscar?text=pollo',
 ]
+
 for url in urls:
     buscar.buscar(url, driver, "Wallmart")
 
