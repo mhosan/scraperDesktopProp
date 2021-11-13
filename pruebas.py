35511e84537f68caef8d01a0ddf06e88460a3b86
marcelo <mhosan@gmail.com>
2021-11-13 19:42:44 -0300

    parece que mas o menos funca Maxi

--- pruebas.py
+++ pruebas.py
@@ -11,41 +11,45 @@ import driver
 driver = driver.seteoDriver()
 
 productoTestigo = "//div[@class='search results']//div[@id='catalog-listing']//ul/li//a[@class='product-item-link']"
-listadoDeProductos = '//div[@class="search results"]//div[@id="catalog-listing"]//ul/li'
-
-stockDisponible = "//div[@class='search results']//div[@id='catalog-listing']//ul/li//div[@class='stock available']/span"
-
-descripcionProducto = "//div[@class='search results']//div[@id='catalog-listing']//ul/li//a[@class='product-item-link']"
-precioProducto = "//div[@class='search results']//div[@id='catalog-listing']//ul/li//div[@class='price-box price-final_price']//span[@class='price']"
+listadoDeProductos = '//div[@class="search results"]//div[@id="catalog-listing"]//ul/li//div[@class="product details product-item-details box-info"]'
+stockDisponible = './/div[@class="name-rating"]//div[@class="stock available"]/span'
+#descripcionProducto = ".//a[@class='product-item-link']"
+precioProducto = ".//div[@class='price-box price-final_price']//span[@data-label='Incl. impuestos']/span[@class='price']"
 
 url = 'https://maxiconsumo.com/sucursal_capital/catalogsearch/result/?q=leche'
 
-driver.get(url)
-# with open(f'paginaVeaLeche.txt', 'w', encoding='utf-8') as f:
-#        f.write(driver.page_source)
-#        f.close()
-listaDeProductos = driver.find_elements_by_xpath(listadoDeProductos)
-print('\n')
-print('=' * 70)
-print(f'La cantidad de productos leidos es: {len(listaDeProductos)}')
-print('=' * 70)
-print('\n')
-if len(listaDeProductos) > 0:
-    fecha = datetime.now()
-    fechaISO = fecha.isoformat()
-    print('\n')
-    print('*' * 70)
-    for producto in listaDeProductos:
-        descripcion = producto.find_element_by_xpath(descripcionProducto).text
-        stock = producto.find_element_by_xpath(stockDisponible).text
-        print(f'descripcion: {descripcion} - stock: {stock}')
-        """ if stock.text == 'Disponible':
-            print('Disponible')
-            precio = producto.find_element_by_xpath(precioProducto).text
-            print(F'Descripcion: {descripcion}, {precio}')
-            print('-' * 70) 
-        else:
-            print(f'{descripcion}: No disponible')
-        """
-    print('*' * 70)
-driver.quit()
+try:
+    driver.get(url)
+    # with open(f'paginaVeaLeche.txt', 'w', encoding='utf-8') as f:
+    #        f.write(driver.page_source)
+    #        f.close()
+    productoVerificar = WebDriverWait(driver, 40).until(EC.presence_of_element_located((By.XPATH, productoTestigo)))
+except TimeoutException:
+    print(f'Tiempo de espera agotado cargando la página "{url}" ')
+    # driver.delete_all_cookies()
+    # driver.quit()
+else:
+    print('\n', '=' * 70)
+    print(f'La página terminó de cargar ok. Driver title: {driver.title}')
+    print('=' * 70, '\n')
+    listaDeProductos = driver.find_elements_by_xpath(listadoDeProductos)
+    listadoDisponibles = driver.find_elements_by_xpath(stockDisponible)
+    print('\n', '=' * 70)
+    print(f'La cantidad de productos leidos es: {len(listaDeProductos)}')
+    print('=' * 70, '\n')
+    if len(listaDeProductos) > 0:
+        fecha = datetime.now()
+        fechaISO = fecha.isoformat()
+        print('\n')
+        print('*' * 70)
+        for i in range(len(listaDeProductos)):
+            producto = listaDeProductos[i]
+            print(f'{i + 1}.- Producto: {listaDeProductos[i].text}')
+            print(f'{i + 1}.- Stock disponible: {listadoDisponibles[i].text}')
+            descripcion = listaDeProductos[i].text
+            disponibilidad = listadoDisponibles[i].text
+            disponibilidad = disponibilidad.strip()
+            if disponibilidad == 'Disponible':
+                precio = producto.find_element_by_xpath(".//div[@class='price-box price-final_price']//span[@data-label='Incl. impuestos']/span[@class='price']").text
+                print(f'Precio: {precio}')
+
